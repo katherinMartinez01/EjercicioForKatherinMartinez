95ed00db548a2e679a5f92014aaa84e173fb80e6
katherin martinez <50372@DESKTOP-B9OA3JG>
2023-03-11 12:26:36 -0600

    Hola

---
+++ EjercicioForKatherinMartinez/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 5; // numero de filas del triangulo
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <= i; j++)
+
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+
+}
+
+
+
